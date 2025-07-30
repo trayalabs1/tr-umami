8169e10903bf55fbf83096d7d3cd68238cdb6869
Nikhil Nayak <nikhilnayak@traya.health>
2025-07-30 18:40:03 +0530

    dummy log

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -85,9 +85,8 @@ async function applyMigration() {
   if (!process.env.SKIP_DB_MIGRATION) {
     console.log(execSync('prisma migrate deploy').toString());
 
-    success('Database is up to date.');
+    success('Database is up to date..');
   }
-  console.log(`Running applicable migrations...`);
 }
 
 (async () => {
